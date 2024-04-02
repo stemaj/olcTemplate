67ea59e7c9f14a8caa23f4044b672ade0d44b34f
Steffen Majaura <steffen.majaura@ebee.de>
2024-04-02 17:37:30 +0200

    FPS sleep

--- game/src/olcTemplate.cpp
+++ game/src/olcTemplate.cpp
@@ -2,7 +2,8 @@
 #include <game/input.hpp>
 #include <game/src/render/render.hpp>
 #include <game/olcTemplate.hpp>
-#include <memory>
+#include <chrono>
+#include <thread>
 
 using namespace stemaj;
 
@@ -11,14 +12,28 @@ bool OlcTemplate::OnUserCreate()
   return true;
 }
 
+void reduceFrameRate(std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<long, std::ratio<1, 1000000000>>> start, float frameTime)
+{
+  auto now = std::chrono::steady_clock::now();
+  auto frameDuration = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count() / 1000.0f;
+  if (frameDuration < frameTime)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>((frameTime - frameDuration) * 1000)));
+  }
+}
+
 bool OlcTemplate::OnUserUpdate(float fElapsedTime)
 {
+  auto frameStartTime = std::chrono::steady_clock::now();
+
   Input input {GetMouseX(),
-    GetMouseY(),
-    GetMouse(0).bPressed};
+  GetMouseY(),
+  GetMouse(0).bPressed};
 
   _game.Update(input);
   _game.Render(this, fElapsedTime);
+
+  reduceFrameRate(frameStartTime, 1.0f/60.f); // 60 FPS
 
   return !GetKey(olc::ESCAPE).bPressed;
 }
