bc1de2452ce735d1b9fec6aca1dc98c532d5d6d7
Steffen Majaura <steffen.majaura@ebee.de>
2024-09-12 15:16:24 +0200

    ffmpeg

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 find . -name '*.cpp' > file_list.txt
 em++ -c -std=c++2a -O2 -I./include/ $(<file_list.txt)
 emar rcs libbox2d.a *.o
+
+### FFMpeg
+
+### Emscripten
+emconfigure ../configure --cc=emcc --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic --disable-asm --disable-debug --disable-doc --disable-ffplay --disable-ffprobe --disable-network --disable-pthreads --disable-x86asm --disable-runtime-cpudetect --disable-iconv --disable-programs --disable-everything --enable-avformat --enable-avcodec --enable-avfilter --enable-swresample --enable-swscale --prefix=./ffmpeg_install --libdir=./ffmpeg_libs
+emmake make -j8
+emranlib *.a
+
+### Linux
+../configure --prefix=./ffmpeg_linux_install --disable-debug --disable-doc --disable-ffplay --disable-ffprobe --disable-network --enable-shared --enable-avformat --disable-programs --enable-avcodec --enable-avfilter --enable-swresample --enable-swscale
--- game/src/videoFrame.cpp
+++ game/src/videoFrame.cpp
@@ -1,9 +1,98 @@
 #include <olcTemplate/game/videoFrame.hpp>
+#include <iostream>
+
+extern "C" {
+    #include <olcTemplate/sdk/ffmpeg-7.0.2/include/libavformat/avformat.h>
+    #include <olcTemplate/sdk/ffmpeg-7.0.2/include/libavcodec/avcodec.h>
+    #include <olcTemplate/sdk/ffmpeg-7.0.2/include/libswscale/swscale.h>
+}
 
 using namespace stemaj;
 
 VideoFrame::VideoFrame(const std::string& filePath)
 {
+  AVFormatContext* fmt_ctx = nullptr;
+    AVCodecContext* codec_ctx = nullptr;
+    AVFrame* frame = nullptr;
+    AVFrame* rgb_frame = nullptr;
+    AVPacket* packet = nullptr;
+    SwsContext* sws_ctx = nullptr;
+    int video_stream_index = -1;
+
+const char* filename;
+    // FFmpeg initialisieren
+        //av_register_all();
+
+        // Video öffnen
+        if (avformat_open_input(&fmt_ctx, filename, nullptr, nullptr) != 0) {
+            std::cerr << "Konnte Video nicht öffnen\n";
+            return;
+        }
+
+        // Streams suchen
+        if (avformat_find_stream_info(fmt_ctx, nullptr) < 0) {
+            std::cerr << "Konnte Stream-Infos nicht finden\n";
+            return;
+        }
+
+        // Videostream finden
+        video_stream_index = -1;
+        for (unsigned int i = 0; i < fmt_ctx->nb_streams; i++) {
+            if (fmt_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+                video_stream_index = i;
+                break;
+            }
+        }
+
+        if (video_stream_index == -1) {
+            std::cerr << "Kein Videostream gefunden\n";
+            return;
+        }
+
+        // Codec für den Videostream öffnen
+        AVCodec* codec = (AVCodec*)avcodec_find_decoder(fmt_ctx->streams[video_stream_index]->codecpar->codec_id);
+        if (!codec) {
+            std::cerr << "Codec nicht gefunden\n";
+            return;
+        }
+
+        codec_ctx = avcodec_alloc_context3(codec);
+        if (avcodec_parameters_to_context(codec_ctx, fmt_ctx->streams[video_stream_index]->codecpar) < 0) {
+            std::cerr << "Konnte Codec-Kontext nicht initialisieren\n";
+            return;
+        }
+
+        if (avcodec_open2(codec_ctx, codec, nullptr) < 0) {
+            std::cerr << "Konnte Codec nicht öffnen\n";
+            return;
+        }
+
+        // Frames und Pakete vorbereiten
+        frame = av_frame_alloc();
+        rgb_frame = av_frame_alloc();
+        packet = av_packet_alloc();
+
+        // SwsContext für die Konvertierung zu RGB erstellen
+        sws_ctx = sws_getContext(codec_ctx->width, codec_ctx->height, codec_ctx->pix_fmt,
+                                 codec_ctx->width, codec_ctx->height, AV_PIX_FMT_RGB24,
+                                 SWS_BILINEAR, nullptr, nullptr, nullptr);
+
+        // Platz für RGB-Frame-Daten reservieren
+        //int num_bytes = av_image_get_buffer_size(AV_PIX_FMT_RGB24, codec_ctx->width, codec_ctx->height, 1);
+        //std::vector<uint8_t> buffer(num_bytes);
+        //av_image_fill_arrays(rgb_frame->data, rgb_frame->linesize, buffer.data(), AV_PIX_FMT_RGB24, codec_ctx->width, codec_ctx->height, 1);
+
+
+
+// Speicher freigeben
+        av_packet_free(&packet);
+        av_frame_free(&frame);
+        av_frame_free(&rgb_frame);
+        avcodec_free_context(&codec_ctx);
+        avformat_close_input(&fmt_ctx);
+        sws_freeContext(sws_ctx);
+
+
   // _videoCapture = std::make_unique<cv::VideoCapture>(filePath);
   // if (!_videoCapture->isOpened())
   // {
