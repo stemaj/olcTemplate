c74c2481459e7325d129dc6054bd0500be1a57a0
Steffen Majaura <steffen.majaura@gmail.com>
2025-02-15 07:33:29 +0100

    room3d stub

---
+++ game/room3d.hpp
@@ -0,0 +1,13 @@
+#ifndef __ROOM_3D
+#define __ROOM_3D
+
+namespace stemaj {
+
+class room3d
+{
+
+};
+
+} // namespace stemaj
+
+#endif // __ROOM_3D
---
+++ game/src/room3d.cpp
@@ -0,0 +1,4 @@
+#include <olcTemplate/game/room3d.hpp>
+
+using namespace stemaj;
+
